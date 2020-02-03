73029d449e4bfba4a9545a39fedf62596e04647d
Kelly Bard <kellyibard@gmail.com>
2020-02-02 18:43:33 -0700

    change route back to welcome from shelters

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'shelters/index'
+
+  get 'welcome/index'
   root 'shelters#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: "shelters#new"
   post "/shelters", to: "shelters#create"
